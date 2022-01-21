086bbe4adac8e0dce4a13e804270af69f5923879
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-01-21 14:35:01 +0100

    fix mistake in default values

--- src/stores/stores.js
+++ src/stores/stores.js
@@ -6,8 +6,8 @@ export const childThemeFolderName = writable('twentytwentyone-child');
 export const webServerType = writable('nginx-fpm');
 export const selectedPhpVersion = writable('7.4');
 export const selectedDbVersionString = writable('maria-10.3');
-export const sshHost = writable('user123');
-export const sshUser = writable('ssh.example.org');
+export const sshUser = writable('user123');
+export const sshHost = writable('ssh.example.org');
 export const sshWpPath = writable('/sites/my-website/wordpress');
 
 export const selectedDbVersionType = derived(selectedDbVersionString, $selectedDbVersionString => $selectedDbVersionString.includes("maria") ? "mariadb_version" : "mysql_version");
