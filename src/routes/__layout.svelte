4faaa47315a788d014bdab6d0373c39f6e7ba633
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-01-21 12:09:50 +0100

    improve footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -12,17 +12,31 @@
 	<slot />
 </main>
 
-<footer class="text-center text-secondary">
+<footer class="text-center text-secondary container ">
 	<hr />
-	Made with SvelteKit, check out at &nbsp;<a
-		href="https://github.com/mandrasch/ddev-pull-wp-generator"
-		target="_blank">GitHub</a
-	>
-	//
-	<a href="https://matthias-andrasch.eu/blog/impressum-datenschutz/" target="_blank"
-		>Imprint & Terms</a
-	> // Stop the climate emergency!
+	<p>
+		Made with SvelteKit, check out at &nbsp;<a
+			href="https://github.com/mandrasch/ddev-pull-wp-generator"
+			target="_blank">GitHub</a
+		>
+		//
+		<a href="https://matthias-andrasch.eu/blog/impressum-datenschutz/" target="_blank"
+			>Imprint & Privacy Statement</a
+		>
+	</p>
+	<p>
+		No personal data or configuration data stored on server. This generator only uses your browser
+		and client-side javascript. See <a
+			href="https://docs.github.com/en/github/site-policy/github-privacy-statement#github-pages"
+			>GitHub Privacy Statement</a
+		> for general data collection on visits.
+	</p>
+	<p>~~~ ! Stop the climate emergency ! ~~~</p>
 </footer>
 
 <style>
+	p {
+		max-width: 40rem;
+		margin: 20px auto;
+	}
 </style>
