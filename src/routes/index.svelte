4fc69225b92e2e0034f3e6df84cf192c264a7406
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-02-05 10:19:20 +0100

    further improvements UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -41,6 +41,7 @@
 <script>
 	import {
 		childThemeFolderName,
+		projectName,
 		providersYamlFromGithub,
 		pullType,
 		sshHost,
@@ -56,9 +57,11 @@
 	import atomOneDark from 'svelte-highlight/src/styles/atom-one-dark';
 	$: sshConnectionTestCmd = `ssh ${$sshUser}@${$sshHost}`;
 	$: sshConnectionTestWithPathCmd = `ssh -t ${$sshUser}@${$sshHost} "cd ${$sshWpPath}; exec \$SHELL --login"`;
-	// this command will use ddev env vars, no need to replace them here
-	const ddevRsyncChildThemeCmd =
-		"ddev exec 'rsync -azh --stats $PRODUCTION_SSH_USER@$PRODUCTION_SSH_HOST:$PRODUCTION_SSH_WP_PATH/wp-content/themes/$CHILD_THEME_FOLDER_NAME/ /var/www/html/${DDEV_DOCROOT}/wp-content/themes/$CHILD_THEME_FOLDER_NAME/'";
+
+	$: ddevRsyncChildThemeCmd = `ddev exec 'rsync -avhz ${$sshUser}@${$sshHost}:${$sshWpPath}/wp-content/themes/${$childThemeFolderName}/ /var/www/html/\${DDEV_DOCROOT}/wp-content/themes/${$childThemeFolderName}/'`;
+
+	$: createDirCmd = `mkdir ${$projectName}
+cd ${$projectName}/`;
 
 	// receive providersYamlFromGithub fetch & save to store
 	export let providersYamlFromGithubFetchResult;
@@ -72,17 +75,31 @@
 			href="https://ddev.readthedocs.io/en/stable/"
 			target="_blank">DDEV</a
 		>
-		project.
+		project. Local development enables stress-free local testing and development - without breaking your
+		live site.
 	</p>
-	<p>
-		Pulling your live site to a local DDEV project allows stress-free local testing and development.
-		For nerdy details see
-		<a
-			href="https://github.com/mandrasch/ddev-pull-wp-scripts"
-			style="font-style:italic"
-			target="_blank">ddev-pull-wp-scripts</a
-		>.
-	</p>
+	<p />
+	<p>Resources:</p>
+	<ul>
+		<li>
+			▶️ <a href="https://www.youtube.com/watch?v=lEGL65H-hts" target="_blank"
+				>SSH connection demo video</a
+			>
+		</li>
+		<li>▶️ Backup file demo - coming soon</li>
+		<li>
+			Source code (DDEV scripts):
+			<a href="https://github.com/mandrasch/ddev-pull-wp-scripts" target="_blank"
+				>ddev-pull-wp-scripts</a
+			>
+		</li>
+		<li>
+			Source code (generator):
+			<a href="https://github.com/mandrasch/ddev-pull-wp-generator" target="_blank"
+				>ddev-pull-wp-generator</a
+			>
+		</li>
+	</ul>
 
 	<div class="alert alert-warning" role="alert">
 		⚠️ This project is work in progress, please use it with caution. Still some bugs to fix. :)
@@ -93,15 +110,6 @@
 	<div class="row">
 		<div class="col-12 px-4 mb-3">
 			<h2>1. Select your configuration</h2>
-			<p>Demo videos of the different pull types can be found here:</p>
-			<ul>
-				<li>
-					▶️ <a href="https://www.youtube.com/watch?v=lEGL65H-hts" target="_blank"
-						>SSH connection demo video</a
-					>
-				</li>
-				<li>▶️ Backup file demo - coming soon</li>
-			</ul>
 
 			<FormWizard />
 		</div>
@@ -112,10 +120,14 @@
 			<h2>2. Setup project folder and copy these files</h2>
 
 			<p>
-				Create a new empty directory for your project. The followings files were generated based on
-				your selected configuration.
+				Create a new empty directory for your project:
+				<Highlight language={shell} code={createDirCmd} />
 			</p>
-			<p>Copy and paste these three files into your newly created project folder:</p>
+
+			<p>
+				The followings files were generated based on your selected configuration. Copy and paste
+				these files into your newly created project folder:
+			</p>
 			<CodeGenerator showOnlyConfigYaml={false} />
 		</div>
 	</div>
@@ -147,8 +159,16 @@
 					<Highlight language={shell} code="ddev pull ssh" />
 				</p>
 
+				<p class="form-text">
+					<span class="badge bg-warning text-dark">Use with caution</span> Please always check in
+					the local <i>wp-config.php</i>-file that the database connection replacement was
+					successfull and that you are connected to the local DDEV project database (and not the
+					remote database of your live site).
+				</p>
+
 				<p>
-					Open the locally cloned site in your browser: <i>ddev launch</i>
+					Optional: Already have a child theme on your live site, but not in your local folder? Pull
+					it in via: <Highlight language={shell} code={ddevRsyncChildThemeCmd} />
 				</p>
 			{/if}
 
@@ -168,13 +188,14 @@
 					<li class="list-group-item">Run "ddev pull backup" to import the backup file</li>
 					<li class="list-group-item">Run "ddev launch" to open your local site in the browser</li>
 				</ol>
-			{/if}
 
-			<p>
-				Please always check in <i>wp-config.php</i> that the database connection replacement was successfull
-				and that you are connected to the local DDEV project database (and not the remote database of
-				your live site).
-			</p>
+				<p class="form-text">
+					<span class="badge bg-warning text-dark">Use with caution</span> Please always check in
+					<i>wp-config.php</i> that the database connection replacement was successfull and that you
+					are connected to the local DDEV project database (and not the remote database of your live
+					site).
+				</p>
+			{/if}
 		</div>
 	</div>
 
@@ -209,22 +230,23 @@
 
 	<div class="row mt-2">
 		<div class="col-12">
-			<h2 class="mb-3">5. Develop, commit, have fun!</h2>
+			<h2 class="mb-3">5. Develop locally & have fun!</h2>
+
+			<p>Now its possible to work with your site locally in a safe way.</p>
 
 			<p>
-				You can now work with your site locally. If you run into issues, please see <a
-					href="https://github.com/mandrasch/ddev-pull-wp-scripts#troubleshooting"
-					target="_blank">Troubleshooting</a
-				>
-				or
-				<a href="https://github.com/mandrasch/ddev-pull-wp-scripts/issues" target="_blank"
-					>create an issue</a
-				>.
+				Open the locally cloned site in your browser:
+				<Highlight language={shell} code="ddev launch" />
+			</p>
+			<p>
+				Open admin dashboard in your browser:
+				<Highlight language={shell} code="ddev launch wp-admin/" />
 			</p>
 
 			{#if $pullType == 'ssh'}
 				<p>
-					Your live sites content changed, new images were added for example? You can pull again:
+					You can always pull again, for example when your live sites content changed and new images
+					were added:
 				</p>
 
 				<Highlight language={shell} code="ddev pull ssh" />
@@ -247,20 +269,20 @@
 				<p>
 					You can push your local child theme folder via SSH (rsync) to your live site. While this
 					is a quick and easy way of deploying for single developers, this can cause trouble in team
-					projects (See alternatives below). Please check your path configuration beforehand and
-					create a backup.
+					projects (See alternatives below).
 				</p>
 
 				<Highlight language={shell} code="ddev push ssh --skip-db" />
+				<p class="form-text">
+					<span class="badge bg-warning text-dark">Use with caution</span> Please create a backup
+					beforehand and make sure your configuration is correctly set in
+					<i>.ddev/providers/ssh.yaml</i>.
+				</p>
 
-				<small style="font-style:italic;">
-					You can as well use 'ddev push ssh' and ignore the warning about the database push - the
-					ssh.yaml-script skips the database_push command. Rsync "--delete" is also disabled
-					currently.
-				</small>
+				<p>Nice, that's all!</p>
 
 				<p class="mt-3">
-					<b>Alternatives for deployment:</b>
+					<b>Alternatives for deployment in team projects (via git):</b>
 				</p>
 				<ul>
 					<li>
